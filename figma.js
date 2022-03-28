78c25745c47309e9ebd10303c24528346541bef2
khinhs <89233809+khinhs@users.noreply.github.com>
2022-03-28 16:06:00 +0800

    Update figma.js

--- figma.js
+++ figma.js
@@ -1,5 +1,19 @@
 console.log('taking note of clicks');
 
+var supportsPassive = false;
+try {
+  var opts = Object.defineProperty({}, 'passive', {
+    get: function() {
+      supportsPassive = true;
+    }
+  });
+  window.addEventListener("testPassive", null, opts);
+  window.removeEventListener("testPassive", null, opts);
+} catch (e) {};
+
+// Use our detect's results. passive applied if supported, capture will be false either way.
+elem.addEventListener('touchstart', fn, supportsPassive ? { passive: true } : false); 
+
 var figmaframe = document.getElementById("figmaUI");
 
 var clickCount = 0;
