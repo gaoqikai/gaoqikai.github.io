02effd22345f936df87e5f9cbea8e42c3489abec
khinhs <89233809+khinhs@users.noreply.github.com>
2022-03-28 15:50:08 +0800

    Create figma.js

---
+++ figma.js
@@ -0,0 +1,11 @@
+console.log('taking note of clicks');
+
+var figmaframe = document.getElementById("figmaUI");
+
+var clickCount = 0;
+
+figmaframe.addEventListener("onclick", function(ev){
+  console.log("clicked!);
+  clickCount ++;
+              }
+
